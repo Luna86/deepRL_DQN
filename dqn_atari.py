84f4a5ceec77becb2c5f36dacabc0a23758379ea
chaoyang <chaoyang@chaoyangs-MBP.wv.cc.cmu.edu>
2017-03-22 19:59:23 -0400

    fix huber loss

--- dqn_atari.py
+++ dqn_atari.py
@@ -22,15 +22,12 @@ from deeprl_hw2.policy import *
 def create_model(window, input_shape, num_actions,
                  model_name='q_network'):  # noqa: D103
     """Create the Q-network model.
-
     Use Keras to construct a keras.models.Model instance (you can also
     use the SequentialModel class).
-
     We highly recommend that you use tf.name_scope as discussed in
     class when creating the model and the layers. This will make it
     far easier to understnad your network architecture if you are
     logging with tensorboard.
-
     Parameters
     ----------
     window: int
@@ -42,7 +39,6 @@ def create_model(window, input_shape, num_actions,
       Number of possible actions. Defined by the gym environment.
     model_name: str
       Useful when debugging. Makes the model show up nicer in tensorboard.
-
     Returns
     -------
     keras.models.Model
@@ -66,18 +62,15 @@ def create_model(window, input_shape, num_actions,
 
 def get_output_folder(parent_dir, env_name):
     """Return save folder.
-
     Assumes folders in the parent_dir have suffix -run{run
     number}. Finds the highest run number and sets the output folder
     to that number + 1. This is just convenient so that if you run the
     same script multiple times tensorboard can plot all of the results
     on the same plots with different names.
-
     Parameters
     ----------
     parent_dir: str
       Path of the directory containing all experiment runs.
-
     Returns
     -------
     parent_dir/run_dir
@@ -112,12 +105,14 @@ def main():  # noqa: D103
     parser.add_argument('--num_burn_in', default=10, type=int, help='number of samples to be filled into the replay memory before updating the network')
     parser.add_argument('--train_freq', default=1, type=int, help='How often to update the Q-network')
     parser.add_argument('--batch_size', default=32, type=int, help='batch_size')
-    parser.add_argument('--num_iterations', default=10000, type=int, help="num of iterations to run for the training")
+    parser.add_argument('--num_iterations', default=50000, type=int, help="num of iterations to run for the training")
     parser.add_argument('--max_episode_length', default=10000, type=int, help='max length of one episode')
     parser.add_argument('--lr', default=0.0001, type=float, help='learning rate')
     parser.add_argument('--epsilon', default=0.05, type=float, help='epsilon for exploration')
     parser.add_argument('--experiment_id', default=None, type=int, help='index of experiment to reload checkpoint')
     parser.add_argument('--save_freq', default=10000, type=int, help='checkpoint saving frequency')
+    parser.add_argument('--evaluate_freq', default=10000, type=int, help='frequency to do evaluation and record video by wrapper')
+    parser.add_argument('--test_num_episodes', default=20, type=int, help='number of episodes to play at each evaluation')
 
     args = parser.parse_args()
 
@@ -149,7 +144,8 @@ def main():  # noqa: D103
 
     #setup DQN agent
     agent = DQNAgent(q_network=model, preprocessor=preprocessor, memory=None, policy=policy, gamma=args.gamma, target_update_freq=args.target_update_freq,
-                     num_burn_in=args.num_burn_in, train_freq=args.train_freq, batch_size=args.batch_size, logdir=args.output, save_freq=args.save_freq)
+                     num_burn_in=args.num_burn_in, train_freq=args.train_freq, batch_size=args.batch_size, logdir=args.output, save_freq=args.save_freq,
+                     evaluate_freq=args.evaluate_freq, test_num_episodes=args.test_num_episodes)
     agent.compile(optimizer=optimizer, loss_func=mean_huber_loss)
     agent.fit(env=game_env, num_iterations=args.num_iterations, max_episode_length=args.max_episode_length)
 
