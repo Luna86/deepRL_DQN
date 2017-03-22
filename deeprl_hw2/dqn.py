3855b9ffed000ddb69ea09fcbb326b20c857b787
chaoyang <chaoyang@chaoyangs-MBP.wv.cc.cmu.edu>
2017-03-22 19:58:31 -0400

    fix huber loss

--- deeprl_hw2/dqn.py
+++ deeprl_hw2/dqn.py
@@ -7,18 +7,15 @@ import numpy as np
 from gym import wrappers
 import tensorflow as tf
 
+
 class DQNAgent:
     """Class implementing DQN.
-
     This is a basic outline of the functions/parameters you will need
     in order to implement the DQNAgnet. This is just to get you
     started. You may need to tweak the parameters, add new ones, etc.
-
     Feel free to change the functions and funciton parameters that the
     class provides.
-
     We have provided docstrings to go along with our suggested API.
-
     Parameters
     ----------
     q_network: keras.models.Model
@@ -45,6 +42,7 @@ class DQNAgent:
     batch_size: int
       How many samples in each minibatch.
     """
+
     def __init__(self,
                  q_network,
                  preprocessor,
@@ -56,7 +54,9 @@ class DQNAgent:
                  train_freq,
                  batch_size,
                  logdir,
-                 save_freq):
+                 save_freq,
+                 evaluate_freq,
+                 test_num_episodes):
         self.model = q_network
         self.preprocessor = preprocessor
         self.memory = memory
@@ -68,20 +68,19 @@ class DQNAgent:
         self.batch_size = batch_size
         self.logdir = logdir
         self.save_freq = save_freq
+        self.evaluate_freq = evaluate_freq
+        self.test_num_episodes = test_num_episodes
 
     def compile(self, optimizer, loss_func):
         """Setup all of the TF graph variables/ops.
-
         This is inspired by the compile method on the
         keras.models.Model class.
-
         This is a good place to create the target network, setup your
         loss function and any placeholders you might need.
-        
+
         You should use the mean_huber_loss function as your
         loss_function. You can also experiment with MSE and other
         losses.
-
         The optimizer can be whatever class you want. We used the
         keras.optimizers.Optimizer class. Specifically the Adam
         optimizer.
@@ -94,10 +93,10 @@ class DQNAgent:
         self.loss = self.loss_func(self.y_true, self.y_pred)
         self.train_op = self.optimizer.minimize(self.loss)
         self.init_op = tf.global_variables_initializer()
-        self.test_reward = tf.placeholder(tf.float32, shape=(), name='test_reward')
+        # self.test_reward = tf.placeholder(tf.float32, shape=(), name='test_reward')
 
-        #set up logger
-        self.reward_summary = tf.summary.scalar('test_reward', self.test_reward)
+        # set up logger
+        # self.reward_summary = tf.summary.scalar('test_reward', self.test_reward)
         self.loss_summary = tf.summary.scalar('loss', self.loss)
         self.merged = tf.summary.merge_all()
         self.file_writer = tf.summary.FileWriter(self.logdir)
@@ -106,9 +105,7 @@ class DQNAgent:
 
     def calc_q_values(self, state):
         """Given a state (or batch of states) calculate the Q-values.
-
         Basically run your network on these states.
-
         Return
         ------
         Q-values for the state(s)
@@ -116,24 +113,18 @@ class DQNAgent:
         q_values = self.sess.run(self.y_pred, feed_dict={self.input: np.expand_dims(state, axis=0)})
         return q_values[0]
 
-
     def select_action(self, q_values, **kwargs):
         """Select the action based on the current state.
-
         You will probably want to vary your behavior here based on
         which stage of training your in. For example, if you're still
         collecting random samples you might want to use a
         UniformRandomPolicy.
-
         If you're testing, you might want to use a GreedyEpsilonPolicy
         with a low epsilon.
-
         If you're training, you might want to use the
         LinearDecayGreedyEpsilonPolicy.
-
         This would also be a good place to call
         process_state_for_network in your preprocessor.
-
         Returns
         --------
         selected action
@@ -141,19 +132,15 @@ class DQNAgent:
 
         return self.policy.select_action(q_values)
 
-
     def update_policy(self):
         """Update your policy.
-
         Behavior may differ based on what stage of training your
         in. If you're in training mode then you should check if you
         should update your network parameters based on the current
         step and the value you set for train_freq.
-
         Inside, you'll want to sample a minibatch, calculate the
         target values, update your network, and then update your
         target values.
-
         You might want to return the loss and other metrics as an
         output. They can help you monitor how training is going.
         """
@@ -161,17 +148,13 @@ class DQNAgent:
 
     def fit(self, env, num_iterations, max_episode_length=None):
         """Fit your model to the provided environment.
-
         Its a good idea to print out things like loss, average reward,
         Q-values, etc to see if your agent is actually improving.
-
         You should probably also periodically save your network
         weights and any other useful info.
-
         This is where you should sample actions from your network,
         collect experience samples and add them to your replay memory,
         and update your network parameters.
-
         Parameters
         ----------
         env: gym.Env
@@ -182,7 +165,7 @@ class DQNAgent:
           How long a single episode should last before the agent
           resets. Can help exploration.
         """
-        #check if checkpoint exists
+        # check if checkpoint exists
         ckpt_dir = os.path.join(self.logdir, 'checkpoints')
         if not os.path.exists(ckpt_dir):
             os.mkdir(ckpt_dir)
@@ -222,49 +205,50 @@ class DQNAgent:
             processed_next_state = self.preprocessor.process_state_for_network(next_state)
             next_q_value = self.calc_q_values(processed_next_state)
 
-            #target: only different at chosen action
+            # target: only different at chosen action
             target = reward + self.gamma * max(next_q_value)
             q_values_target = np.array(q_values)
             q_values_target[action] = target
 
-            loss_summary, loss, _ = self.sess.run([self.loss_summary, self.loss, self.train_op], feed_dict={self.y_true:np.expand_dims(q_values_target, axis=0), self.y_pred: np.expand_dims(q_values, axis=0), self.input: np.expand_dims(processed_state, axis=0)})
+            loss_summary, loss, _ = self.sess.run([self.loss_summary, self.loss, self.train_op],
+                                                  feed_dict={self.y_true: np.expand_dims(q_values_target, axis=0),
+                                                             self.y_pred: np.expand_dims(q_values, axis=0),
+                                                             self.input: np.expand_dims(processed_state, axis=0)})
             duration = time.time() - start_time
             if i % 50 == 0:
-                #print('max next q:{0}'.format(max(next_q_value)))
-                #print('target:{0}'.format(target))
-                #print('reward: {0}'.format(reward))
-                #print('q_values - next_q_values: {0}'.format(max(abs(q_values - next_q_value))))
-                #print('action:{0}'.format(action))
-                #print('q_target: {0}'.format(q_values_target))
+                # print('max next q:{0}'.format(max(next_q_value)))
+                # print('target:{0}'.format(target))
+                # print('reward: {0}'.format(reward))
+                # print('q_values - next_q_values: {0}'.format(max(abs(q_values - next_q_value))))
+                # print('action:{0}'.format(action))
+                # print('q_target: {0}'.format(q_values_target))
                 print('q_values: {0}'.format(q_values))
-                #print('q_values - q_target: {0}'.format(max(abs(q_values - q_values_target))))
+                # print('q_values - q_target: {0}'.format(max(abs(q_values - q_values_target))))
                 print('iter= {0}, loss = {1:.4f}, ({2:.2f} sec/iter)'.format(i, loss, duration))
                 print()
-                #reward_summary = self.sess.run([self.reward_summary], feed_dict={self.test_reward: average_test_reward})
-                #self.file_writer.add_summary(reward_summary, i)
+                # reward_summary = self.sess.run([self.reward_summary], feed_dict={self.test_reward: average_test_reward})
+                # self.file_writer.add_summary(reward_summary, i)
             if i > 0 and i % self.save_freq == 0:
                 save_dir = os.path.join(self.logdir, 'checkpoints', str(i))
                 self.model.save_weights(save_dir)
                 print("Saving model at {0}".format(save_dir))
-            if i > 0 and i % (num_iterations / 3) == 0:
-                average_test_reward = self.evaluate(env=gym.make('SpaceInvaders-v0'), num_episodes=1, iter=i)
+            if i > 0 and i % self.evaluate_freq == 0:
+                average_test_reward = self.evaluate(env=gym.make('SpaceInvaders-v0'),
+                                                    num_episodes=self.test_num_episodes, iter=i)
                 print('Evaluation at iter {0}: average reward for 20 episodes: {1}'.format(i, average_test_reward))
 
             self.file_writer.add_summary(loss_summary, i)
             state = next_state
             iter_epi += 1
 
-
     def evaluate(self, env, num_episodes, iter):
         """Test your agent with a provided environment.
-        
+
         You shouldn't update your network parameters here. Also if you
         have any layers that vary in behavior between train/test time
         (such as dropout or batch norm), you should set them to test.
-
         Basically run your policy on the environment and collect stats
         like cumulative reward, average episode length, etc.
-
         You can also call the render function here if you want to
         visually inspect your policy.
         """
